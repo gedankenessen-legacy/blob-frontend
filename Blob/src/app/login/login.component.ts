a83098b1ee5dfb1880aaebcd97617710fd83d37f
Benjamin Kürten <benjaminkuerten@gmail.com>
2020-05-11 19:43:22 +0200

    + Login + MainLayout

--- Blob/src/app/login/login.component.ts
+++ Blob/src/app/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.less']
+  styleUrls: ['./login.component.less'],
 })
 export class LoginComponent implements OnInit {
+ 
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required])
+    }); 
   }
 
+  submitForm() {
+
+  }
 }
