7cb2d1b5bc3ca8c8f1004076dd29d828b73d0f79
Philipp Heim <philipp-heim@outlook.de>
2020-05-11 16:35:46 +0200

    + Created all modules

---
+++ Blob/src/app/statstic/statstic.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StatsticRoutingModule } from './statstic-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StatsticRoutingModule
+  ]
+})
+export class StatsticModule { }
