ccb7598bc666853c02cb0e22ee0f0105e12fe095
Philipp Heim <philipp-heim@outlook.de>
2020-05-12 12:13:16 +0200

    + Auth-Service: Added isAuth/signIn/out mehtod + Auth-Guard: Added logic for checking if the user can use this route, otherwise signout and redirect to login

--- Blob/src/app/auth/auth.guard.ts
+++ Blob/src/app/auth/auth.guard.ts
@@ -1,17 +1,29 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate, CanActivateChild {
+  constructor(private authService: AuthService, private router: Router) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    // Check if authenticated, if not redirect to login
+    if (this.authService.isAuthenticated) {
+      this.router.navigate(['/login']);
+      return true;
+    }
+
+    // Sign the user out and redirect.
+    this.authService.signOut();
+    return false;
   }
+
   canActivateChild(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
--- Blob/src/app/auth/auth.service.ts
+++ Blob/src/app/auth/auth.service.ts
@@ -4,5 +4,46 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class AuthService {
+  /**
+   * Checks if the user is Authenticated.
+   * @returns True if the user is Authenticated.
+   */
+  public get isAuthenticated(): boolean {
+    return this.isTokenValid();
+  }
+
   constructor() {}
+
+  /**
+   * This method checks if the Token is expired.
+   * @returns   'true' if the token is still valid, otherwise 'false'.
+   */
+  private isTokenValid(): boolean {
+    // TODO: Check if the token is expired
+    return true; //! DEBUG: only for debugging!
+
+    return false;
+  }
+
+  /**
+   * This method is used to authenticate a user and storing a token, which is need to call resources from the API.
+   * @param username The username.
+   * @param password The password.
+   */
+  public signIn(username: string, password: string): boolean {
+    // TODO: Create a http req on the auth api.
+    // TODO: Store the token if the req was successful.
+    // TODO: Redirect to homepage.
+    return true; //! DEBUG: only for debugging!
+  }
+
+  /**
+   * This method signs the user out, deletes the stored token and redirect the user to the Login-Page.
+   * @returns 'true' if the signOut was successful.
+   */
+  public signOut(): boolean {
+    // TODO: Remove the stored token from the browser storage.
+    // TODO: Redirect to "/login".
+    return true; //! DEBUG: only for debugging!
+  }
 }
