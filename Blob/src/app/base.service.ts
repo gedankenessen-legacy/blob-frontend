41d8be302025865792561175bd8584a5cf4e7511
sfuerder <sfuerder@stud.hs-offenburg.de>
2020-06-06 10:27:06 +0200

    + created the service # changed the ILocationItem

---
+++ Blob/src/app/base.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BaseService {
+  private baseUrl: string = 'https://localhost:5001/api';
+
+  constructor() {}
+
+  public get getBaseUrl(): string {
+    return this.baseUrl;
+  }
+
+  /**
+   * Method for services to handle errors.
+   * @param error The error obj.
+   */
+  public errorHandle(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status} Message: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
+}
--- Blob/src/app/interfaces/manage/ILocationItem.ts
+++ Blob/src/app/interfaces/manage/ILocationItem.ts
@@ -1,7 +1,7 @@
+import { IAdress } from '../IAdress';
+
 export interface ILocationItem {
   id: number;
   name: string;
-  street: string;
-  zip: string;
-  city: string;
+  address: IAdress;
 }
