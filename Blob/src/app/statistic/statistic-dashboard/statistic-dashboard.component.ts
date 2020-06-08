e5c911c54a5b3e8f9c3140e4e4d6785de25612ed
Philipp Heim <philipp-heim@outlook.de>
2020-06-08 15:33:53 +0200

    + ngx-charts imports and basic area line chart

--- Blob/src/app/statistic/statistic-dashboard/statistic-dashboard.component.ts
+++ Blob/src/app/statistic/statistic-dashboard/statistic-dashboard.component.ts
@@ -7,9 +7,33 @@ import { TitleService } from 'src/app/title.service';
   styleUrls: ['./statistic-dashboard.component.less'],
 })
 export class StatisticDashboardComponent implements OnInit {
+  multi: any[];
+  view: any[] = [700, 300];
+
+  // options
+  legend: boolean = true;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Year';
+  yAxisLabel: string = 'Population';
+  timeline: boolean = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5'],
+  };
+
   constructor(private titleService: TitleService) {
     this.titleService.Title = 'Statistik';
+    Object.assign(this, { multi: this.multi });
   }
 
   ngOnInit(): void {}
+
+  onSelect(event) {
+    console.log(event);
+  }
 }
--- Blob/src/app/statistic/statistic.module.ts
+++ Blob/src/app/statistic/statistic.module.ts
@@ -5,9 +5,10 @@ import { StatisticRoutingModule } from './statistic-routing.module';
 import { StatisticDashboardComponent } from './statistic-dashboard/statistic-dashboard.component';
 
 import { NzGridModule } from 'ng-zorro-antd';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [StatisticDashboardComponent],
-  imports: [CommonModule, StatisticRoutingModule, NzGridModule],
+  imports: [CommonModule, StatisticRoutingModule, NzGridModule, NgxChartsModule],
 })
 export class StatisticModule {}
